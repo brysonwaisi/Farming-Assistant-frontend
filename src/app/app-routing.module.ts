5ebe1251cea59e4c950b24fa16dc001d6935327e
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2020-10-20 10:52:20 +0300

    addedouter links and modified the app-routing module by removing some imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { ProductComponent } from './components/product/product.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
-import { ThankyouComponent } from './components/thankyou/thankyou.component';
 
 
-const routes: Routes= [
-{path: '', component: HomeComponent},
-{path: 'product/:id', component: ProductComponent},
-{path: 'cart', component: CartComponent},
-{path: 'checkout', component: CheckoutComponent},
-{path:  'thankyou', component: ThankyouComponent},
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent }
 ]
 
 @NgModule({
