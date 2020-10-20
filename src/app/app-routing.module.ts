5eab63845e7979aa4bd1633fa4484c621d245106
Kevin Muriuki <kevinmuriukiwaweru@gmail.com>
2020-10-20 11:41:40 +0300

    modified an error in the home.component.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './components/home/home.component';
 import { CartComponent } from './components/cart/cart.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
-
-
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
